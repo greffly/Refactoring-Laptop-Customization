56331ff880e17b6712a674538fbb5e50fbbb2848
Caitlyn <cgreffly@gmail.com>
2019-07-07 13:37:41 -0700

    add docs

---
+++ docs/precache-manifest.2405307baa34545db85373843b0c7de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "aacc8e3ec811b255fd84",
+    "url": "/static/js/main.aacc8e3e.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "aacc8e3ec811b255fd84",
+    "url": "/static/css/main.6c8fbf42.chunk.css"
+  },
+  {
+    "revision": "506ea1da9078725b906d67efa594ff09",
+    "url": "/index.html"
+  }
+];
